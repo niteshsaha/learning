e608fd896089468438d55025b33fa60ebb6bbc69
niteshsaha <nitesh.rokz@gmail.com>
2020-09-16 19:40:09 +0530

    Update Binary_Search_all.py

--- Binary_Search_all.py
+++ Binary_Search_all.py
@@ -138,4 +138,98 @@ index = findInsertionIndex(arry6,target)
 print 'We can insert in position %d in arry [1,2,4,4,5,6,8]'%index
 
 
+######### Search Array of Unknown length
+## You are given an array, but you don't know the length. Write a program to find a target element in the array.
+########
+
+print 'Search Array of Unknown lengt'
+
+def findTarget(a,target,start,end):
+   while(start <= end):
+        mid = start + (end - start)/2
+        if (a[mid] < target):
+                start = mid +1
+        elif (a[mid] > target):
+                end = mid - 1
+        else:
+                return  mid
+   return -1
+
+def findArrayOutofBoundRange(array, inRange,outRange):
+        while(inRange<= outRange):
+                mid = inRange + (outRange - inRange)/2
+                try:
+                        tmp = array[mid]
+                except:
+                        outRange = mid - 1
+                        continue
+                try:
+                        tmp = array[mid +1]
+                except:
+                        return mid
+                inrange = mid + 1
+        return -1 # This should never happen
+
+def targetInUnknowArrayLen(array,target):
+        end = 1
+        while True:
+                try:
+                        temp = array[end]
+                except:
+                        lastIndex = findArrayOutofBoundRange(array,end/2,end)
+                        break
+                end = (end * 2)
+
+        return findTarget(array, target, 0 , lastIndex)
+
+array10 = [10,1,3,4,6,7,8,9,12]
+index  = targetInUnknowArrayLen(array10,7)
+print 'Target found in position : %d'%index
+
+##### Find the square root of an integer X. For example, squareRoot(4) = 2 If X is not a perfect square, find the integer floor of the square root
+
+print 'Find the square root of an integer X. For example, squareRoot(4) = 2'
+
+def squareRoot(num):
+        start =0; end =num/2
+        while start <= end:
+                mid = start + (end - start)/2
+                if (mid * mid) > num:
+                        end = mid - 1
+                elif (mid * mid) < num:
+                        if ((mid + 1 ) * (mid + 1)) > num:
+                                return mid
+                        start = mid +1
+                else:
+                        return mid
+        return -1
+
+num = int(raw_input('Enter number to find the square root : '))
+print 'square root of given number (%d) is  : %d'%(num, squareRoot(num))
+
+##### Search for a Peak
+
+print 'Search for a Peak element left and right of that element should be less than of that element'
+
+def peakElement(array):
+        low = 0; high = len(array) -1
+        while low <= high:
+                mid = low + (high - low)/2
+                left = (mid -1) if (mid > 0) else -math.inf
+                right = (mid + 1 ) if (mid < len(array) - 1) else math.inf
+                print left, right
+                if array[left] < array[mid] and array[mid] < array[right]:
+                        low = mid +1
+                elif array[left] > array[mid] and array[mid] > array[right]:
+                        high = mid - 1
+                elif array[left] > array[mid] and array[mid] < array[right]:
+                        high = mid -1  # we can traverse in either way
+                else:
+                        return mid
+        return -1 # This should never happen.
+
+
+array11 = [1,3,4,5,2]
+ele = peakElement(array11)
+print 'peak element of array11 %d'%array11[ele]
 
