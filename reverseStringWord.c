85246ee70d11ca4ced924c7adc4efa109c0dec08
niteshsaha <nitesh.rokz@gmail.com>
2020-10-07 10:13:44 +0530

    Update reverseStringWord.c

--- reverseStringWord.c
+++ reverseStringWord.c
@@ -1,9 +1,7 @@
 /******************************************************************************
-
 Given a sentence, reverse the words of the sentence.
 For example,
 "i live in a house" becomes "house a in live i"
-
 *******************************************************************************/
 
 #include <stdio.h>
@@ -11,41 +9,39 @@ For example,
 
 int main()
 {
-    char str[] = "i live in a house";
+    char str[] = "nitesh i live in a house";
     int strSize= strlen(str);
     
     printf("Lenght of string : %d",strSize);
     
     char str1[strSize];
     
-    int startWord=0,endWord=strSize-1,i,k=0;
+    int startWord =0, endWord=strSize-1,i,k=0,j;
     
-    while(startWord != -1) {
-        /*search the start word*/
-        for(i=endWord;i>=0;i--) {
-            if(str[i] == ' ') {
-                 startWord = i+1;
-                 break;
-             }
-        }
-        /* copy the word in new array*/
-        for(i=startWord; i <=endWord;i ++ ) {
-            str1[k] = str[i];
-            k++;
-        }
-        if(startWord-2 >=0) {
-            str1[k] = ' ';
-            k++;
-            endWord = startWord-2;
-            startWord =0;
-        } else {
-            startWord = -1;
+    for(i=strSize-1;i>=0;i--) {
+        if(str[i] == ' ') {
+            startWord = i + 1;
+            for(j=startWord;j<=endWord;j++) {
+                str1[k] = str[j];  
+                k++;
+            }
+            if(startWord-2 >= 0) {
+                str1[k] = ' ';
+                k++;
+                endWord = startWord-2;
+                startWord = 0;
+            }
+        } else if (i == 0) {
+            for(j=i;j<=endWord;j++) {
+                str1[k] = str[j];  
+                k++;
+            }
         }
     }
     
-    //print the new array
+        //print the new array
     printf("\n orginal string : %s",str);
     printf("\n final str is : %s",str1);
    
-    return 0;
+    
 }
